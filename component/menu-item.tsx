3c9e07fff15101ac97e9d0fcb28ccc9b1cf5eb76
q-0q <jrwthrs@gmail.com>
2025-10-20 23:30:19 -0400

    feat: synched animations

--- component/menu-item.tsx
+++ component/menu-item.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { CSSProperties, useContext } from "react";
+import { CSSProperties, useContext, useEffect, useRef } from "react";
 import { menuItems } from "@/data/menu-data";
 import { useState } from "react";
 import { useAppContext } from "./context";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 type MenuItemProps = {
   index: number;
@@ -15,14 +17,29 @@ type MenuItemProps = {
 
 export default function MenuItem({ index, title, description, link }: MenuItemProps) {
 
+    const ref = useRef<HTMLDivElement>(null);
+
     const { selectedPageIndex, setSelectedPageIndex} = useAppContext()
 
     function handleClick(){
         setSelectedPageIndex(index)
     }
 
+    useEffect(() => {
+        if (selectedPageIndex === index) {
+            gsap.to(ref.current, {
+                x: 60
+    
+            })
+        } else {
+            gsap.to(ref.current, {
+                x: 0,
+            });
+        }
+    });
+
     return (
-      <div style={styles.container}>
+      <div style={styles.container} ref={ref}>
         <div style={styles.shape} onClick={handleClick} />
         <div style={styles.text}>
           <p onClick={handleClick}>{title}</p>
